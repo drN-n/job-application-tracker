6eff4a28a278a6a11796afbc2821e8b3f4fd1f37
Aldrin Mangubat <amangubat18@gmail.com>
2026-09-02 13:08:08 +0800

    Initial setup and project structure with Spring Boot backend

---
+++ backend/src/main/java/com/drn/job_application_tracker_api/JobApplicationTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.drn.job_application_tracker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackerApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/drn/job_application_tracker_api/JobApplicationTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drn.job_application_tracker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
